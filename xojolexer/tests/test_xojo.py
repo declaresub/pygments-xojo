df93a3cb0f320e37fed5cf480c4744ac712aea54
Charles <charles@declareSub.com>
2015-07-24 18:44:57 -0400

    Parameterize test for future additions.
    Signed-off-by: Charles <charles@declareSub.com>

--- xojolexer/tests/test_xojo.py
+++ xojolexer/tests/test_xojo.py
@@ -3,10 +3,29 @@
 """Unit tests for xojo module."""
 
 from __future__ import absolute_import
+import pytest
+from pygments.token import *
 from xojolexer.xojo import XojoLexer
 
 def test_smoke():
     """A basic smoke test."""
     
     assert XojoLexer()
+
+
+
+@pytest.mark.parametrize("source, tokens", 
+    [
+    ('x = 1', [
+        (Token.Name.Variable, u'x'),
+        (Token.Text, u' '),
+        (Token.Operator, u'='),
+        (Token.Text, u' '),
+        (Token.Literal.Number.Integer, u'1'),
+        (Token.Text, u'\n')
+        ]),
     
+    ])
+def test_lexing(source, tokens):    
+    lexer = XojoLexer()
+    assert [t for t in lexer.get_tokens(source)] == tokens
