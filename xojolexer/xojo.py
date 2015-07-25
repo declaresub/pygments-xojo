c3b141f06da38f3556680369a1e5e6cd3c87020d
Charles <charles@declareSub.com>
2015-07-25 11:33:40 -0400

    Add class _FunctionParamsLexer.
    Signed-off-by: Charles <charles@declareSub.com>

--- xojolexer/xojo.py
+++ xojolexer/xojo.py
@@ -11,11 +11,57 @@ Lexer for the Xojo language.
 
 from __future__ import absolute_import
 import re
-from pygments.lexer import RegexLexer, words, include
+from pygments.lexer import RegexLexer, words, include, default
 from pygments.token import Keyword, Name, String, Literal, Number, Punctuation, Comment, \
     Operator, Text
 
 __all__ = ['XojoLexer']
+
+
+# various regular expressions
+IDENTIFIER = r'[^\d\W]\w*'
+
+
+
+
+class _DeclareSubLexer(RegexLexer):
+    pass
+               
+                            
+class _DeclareFunctionLexer(RegexLexer):
+    pass
+    
+class _FunctionParamsLexer(RegexLexer):
+    name = 'xojofunctionparam'
+    aliases = ['xojofunctionparam']
+    flags = re.IGNORECASE | re.UNICODE     
+    
+    tokens = {
+        'ignorable_whitespace': [
+            (r'[\ \t]+', Text),
+            ],
+            
+        'root': [
+            include('ignorable_whitespace'),
+            (IDENTIFIER, Name.Variable, 'param_as'), 
+            ],        
+    
+        'param_as': [
+            include('ignorable_whitespace'),
+            (r'as', Keyword.Reserved, 'param_type')
+            ],
+            
+        'param_type': [
+            include('ignorable_whitespace'),
+            (IDENTIFIER, Name, 'param_sep'), 
+            ],
+
+        'param_sep': [
+            include('ignorable_whitespace'),
+            (r'\,', Punctuation, 'root'),
+            default('#pop'),
+            ],
+        }
 
 class XojoLexer(RegexLexer):
     """Lexer for Xojo."""
@@ -26,6 +72,10 @@ class XojoLexer(RegexLexer):
 
     #pylint: disable=bad-continuation
     IDENTIFIER = r'[^\d\W]\w*'
+    IDENTIFIER_FQ = r'[^\d\W]\w*(\.[^\d\W]\w*)*'
+    LITERAL_STRING = r'"(""|[^"])*"'
+    LITERAL_UNICODE = r'&u[0-9a-fA-F]+'
+    
     BUILTINS = ('AddHandler', 'Call','CurrentMethodName',
     'Raise',  'RemoveHandler')
     KEYWORDS = ('Aggregates', 'As', 'Assigns', 'Attributes', 'Break', 'ByRef', 'ByVal',
@@ -33,7 +83,7 @@ class XojoLexer(RegexLexer):
     'Enum', 'Else', 'ElseIf', 'End', 'Event', 'Exception', 'Exit', 'Extends', 'Finally',
     'For', 'Function', 'Global', 'Handles', 'If', 'Implements', 'In', 'Inherits', 'Interface',
     'Lib', 'Loop', 'Module', 'Next', 'New', 'Namespace', 'Optional', 'ParamArray', 'Private',
-    'Protected', 'Public', 'Return', 'Select', 'Selector', 'Soft', 'Step', 'Structure',
+    'Protected', 'Public', 'Return', 'Select', 'Selector', 'Step', 'Structure',
     'Sub', 'Then', 'To', 'Try', 'Until', 'Wend', 'While', 'With', 'WithEvents',
     '#if', '#else', '#endif', '#pragma')
     OPERATOR_WORDS = ('And', 'Is', 'IsA', 'Mod', 'Not', 'Or', 'Xor', 'AddressOf', 'Array',
@@ -49,14 +99,14 @@ class XojoLexer(RegexLexer):
             (words(('const', 'dim', 'static'), suffix=r'\b'), Keyword.Declaration),
             (words(('false', 'nil', 'true'), suffix=r'\b'), Keyword.Constant),
             (words(BUILTINS, suffix=r'\b'), Name.Builtin),
-            (words(('Using',), suffix=r'\b'), Keyword.Namespace, 'using_ns'),
+            (words(('Using', 'module'), suffix=r'\b'), Keyword.Namespace, 'namespace_decl'),
             (words(('GOTO',), suffix=r'\b'), Keyword.Reserved, 'goto'),
             (words(('self', 'me', 'super'), suffix=r'\b'), Name.Builtin.Pseudo),
             (words(KEYWORDS, suffix=r'\b'), Keyword.Reserved),
 
             # Literals
-            (r'"(""|[^"])*"', String),
-            (r'&u[0-9a-fA-F]+', Literal),
+            (LITERAL_STRING, String),
+            (LITERAL_UNICODE, Literal),
             (r'[0-9]+', Number.Integer),
             (r'&b[01]+', Number.Bin),
             (r'&o[0-7]+', Number.Oct),
@@ -77,7 +127,9 @@ class XojoLexer(RegexLexer):
             (IDENTIFIER, Name.Variable),
             ],
 
-        'using_ns': [
+
+
+        'namespace_decl': [
             include('ignorable_whitespace'),
             (r'[^\d\W]\w*(.[^\d\W]\w*)*', Name.Namespace, '#pop'),
             ],
@@ -86,4 +138,15 @@ class XojoLexer(RegexLexer):
             include('ignorable_whitespace'),
             (IDENTIFIER, Name.Label, '#pop'),
             ],
+
+       
+         
+         #use multiple lexers to read declare sub v. function statements.  
+
+
+            
+
+            
         }
+        
+
