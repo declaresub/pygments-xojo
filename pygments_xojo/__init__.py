24fc5530f292a63cbb2782b0ce7b3ffc4976180b
Charles <charles@declareSub.com>
2015-07-26 15:17:04 -0400

    Satisfy pylint.
    Signed-off-by: Charles <charles@declareSub.com>

--- pygments_xojo/__init__.py
+++ pygments_xojo/__init__.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
-"""This version is also read by setup.py."""
+
+"""Pygments syntax highlighting for Xojo."""
 
 
 from pygments.token import Token
+# the point of the next statement is that accessing the attribute creates the token type,
+# as explained in the pygments token documentation.
+#pylint:disable=pointless-statement
 Token.Name.XojoType
 
-
+# __version is also read by setup.py.
 __version__ = '0.0.0'
