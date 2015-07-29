f65ec17ac2c467b6e124e2768c0290ff506c108b
Charles <charles@declareSub.com>
2015-07-29 18:26:41 -0400

    Rewrite script to use pyvenv when available; script now installs pip from pypi if needed.
    Signed-off-by: Charles <charles@declareSub.com>

--- make_scripts/venv.bash
+++ make_scripts/venv.bash
@@ -7,30 +7,42 @@
 
 PIP="pip"
 VIRTUALENV="virtualenv"
-ENV_LIST="python2 python3"
+PYVENV="pyvenv"
+PYVERSIONS="2.6 2.7 3.3 3.4"
 ENV_EXT="venv"
 
-#let's make sure that pip and virtualenv are installed.
-if ! hash "$PIP" 2>/dev/null; then
-    echo "The ${PIP} command was not found." >&2
-    exit 1
-fi
 
-if ! hash "$VIRTUALENV" 2>/dev/null; then
-    echo "The ${VIRTUALENV} command was not found." >&2
-    exit 1
-fi
-
-for ENV in $ENV_LIST;
+for VERSION in $PYVERSIONS;
     do
-    if hash "$ENV" 2> /dev/null; then
-        ENV_DIR="$ENV.$ENV_EXT"
-        echo "Creating directory $ENV_DIR"
-        rm -rf "$ENV_DIR"
-        mkdir "$ENV_DIR"
+    #first, look for pyvenv; it should be available for $VERSION >= '3.3.
+    
+    
+    VENV="$PYVENV-$VERSION"
+    if ! hash "$VENV" 2> /dev/null; then
+        #not found, so fall back to virtualenv.
+        VENV="$VIRTUALENV"
+        VENV_ARGS="--python=python$VERSION --clear "
+    else
+        VENV_ARGS="--clear"
+    fi
+    if hash "$VENV" 2> /dev/null; then
+        ENV_DIR="python$VERSION.$ENV_EXT"
+        #echo "Creating directory $ENV_DIR"
+        #rm -rf "$ENV_DIR"
+        #mkdir "$ENV_DIR"
         echo "Initializing virtualenv."
-        virtualenv --python="$ENV" "$ENV_DIR"
+        "$VENV" $VENV_ARGS "$ENV_DIR"
         source "$ENV_DIR/bin/activate"
+        
+        #check for pip install in this virtual environment.
+        if [[ ! -x $VIRTUAL_ENV/bin/pip ]]; then
+            echo "pip is not included in this virtual environment. Installing pip."
+            if [[ ! =f get-pip.py ]]; then
+                curl -o get-pip.py https://bootstrap.pypa.io/get-pip.py
+            fi
+            python get-pip.py   
+        fi
+        
         echo "Installing this package."
         pip install --process-dependency-links  --editable .
         echo "Installing additional packages from requirements.txt."
@@ -38,3 +50,4 @@ for ENV in $ENV_LIST;
         deactivate
     fi  
     done
+    rm -f get-pip.py
